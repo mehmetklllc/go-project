4a82d6d4b255dc9a803a3174a2d77b402b31a331
Mehmet KILIÇ <mehmetklllllc@gmail.com>
2020-11-03 02:32:17 +0300

    Update jsonUtil.go

--- go-custom-struct/jsonUtil.go
+++ go-custom-struct/jsonUtil.go
@@ -2,11 +2,11 @@ package mkilic
 
 import (
 	"encoding/json"
-	
+	"github.com/mehmetklllc/go-project/go-custom-struct"
 	"log"
 )
 
-func ObjectTojson(device Device) string {
+func ObjectTojson(device *mkilic.Device) string {
 
 	var jsonData []byte
 	jsonData,err := json.Marshal(device)
@@ -17,9 +17,9 @@ func ObjectTojson(device Device) string {
 	return string(jsonData)
 }
 
-func JsonToObject(jsonData []byte) Device {
+func JsonToObject(jsonData []byte) *mkilic.Device {
 
-	var unDevice Device
+	var unDevice mkilic.Device
 	_ = json.Unmarshal(jsonData, &unDevice)
 
 	return unDevice
