22f97b0f38117744abf45033f1a748959ac25528
Mehmet KILIÇ <mehmetklllllc@gmail.com>
2020-11-03 02:19:06 +0300

    Add files via upload

---
+++ go-custom-struct/jsonUtil.go
@@ -0,0 +1,26 @@
+package mkilic
+
+import (
+	"encoding/json"
+	"github.com/mehmetklllc/go-project/go-custom-struct"
+	"log"
+)
+
+func ObjectTojson(device mkilic.Device) string {
+
+	var jsonData []byte
+	jsonData,err := json.Marshal(device)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return string(jsonData)
+}
+
+func JsonToObject(jsonData []byte) mkilic.Device {
+
+	var unDevice mkilic.Device
+	_ = json.Unmarshal(jsonData, &unDevice)
+
+	return unDevice
+}
